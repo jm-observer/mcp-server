d0ee4fe9d57f4730a099931dbf7a152947f4de44
fengqi <362254883@qq.com>
2026-03-19 20:51:36 +0800

    update

--- src/config/schema.rs
+++ src/config/schema.rs
@@ -1,7 +1,13 @@
 use schemars::schema_for;
+use crate::config::ToolDef;
 use super::tool::ToolFile;
 
-pub fn tool_config_schema() -> String {
+pub fn tool_file_config_schema() -> String {
     let schema = schema_for!(ToolFile);
+    serde_json::to_string_pretty(&schema).unwrap()
+}
+
+pub fn tool_config_schema() -> String {
+    let schema = schema_for!(ToolDef);
     serde_json::to_string_pretty(&schema).unwrap()
 }
